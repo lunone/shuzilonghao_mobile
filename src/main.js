b56c2120868298c0abebe6a627bf7fe89b0dcd71
lunone <lunone@qq.com>
2025-01-23 16:18:47 +0800

    恢复mainjs的条件编译

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import App from './App'
 import * as Pinia from 'pinia';
 
-// // #ifndef VUE3
-// import Vue from 'vue'
-// import './uni.promisify.adaptor'
-// Vue.config.productionTip = false
-// App.mpType = 'app'
-// const app = new Vue({
-//     ...App
-// })
-// app.$mount()
-// // #endif
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue';
