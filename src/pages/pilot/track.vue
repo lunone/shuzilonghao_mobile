613384625125661fa268bb468d34e5aca93bb3a6
zhilun <lunone@qq.com>
2025-03-12 14:53:02 +0800

    美化track

--- src/pages/pilot/track.vue
+++ src/pages/pilot/track.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flight-tracks-wrapper">
         <div v-for="day in days" :key="day.index" class="day" :class="day.className" @click="showDetail(day)">
-            <span>{{ day.name }}</span>
+            <i v-if="day.name" :class="`zl-icon-${day.name}`" />
+            <span v-else>{{ day.index }}</span>
         </div>
     </div>
 </template>
@@ -22,6 +23,17 @@ const duties = ref([]) as Ref<{ arr: string, dep: string, flightDate: Date, flig
 const absences = ref([]) as Ref<{ code: string, ddoCode: string, ddoType: string, startDate: Date, endDate: Date, title: string, detail: string, userId: string }[]>;
 
 const airportStore = useAirportStore();
+const absenceReasons = {
+    grounded: ['技术资质停飞', '安全停飞', '行政停飞', '其他停飞', '辞职停飞', '身体原因停飞', '停飞'],
+    vacation: ['3天非隔离休息', '月假', '陪产假', '疗养假', '事假', '隔离'],
+    learn: ['授课', '理论学习', '班组线下培训', '中国民航管理干部学院培训', '安保培训', '飞行员线下培训',
+        '差异理论培训', '部门培训', '部门培训补训', '定期地面理论学习', '飞管线下培训', '班组及飞管线下培训'],
+    job: ['飞行值班', 'D1', '外站过夜', '出差'],
+    meeting: ['会议', '排练'],
+    foc: ['分部运行需求-占位', '个人运行需求-占位', '分部运行需求-不占位', '个人运行需求-不占位'],
+    hospital: ['体检', '体检复查'],
+    more: ['其他活动'],
+};
 
 // 日历
 const days = computed(() => {
@@ -31,27 +43,35 @@ const days = computed(() => {
     // console.log('00000000000', dayLength);
     for (let i = 0; i < dayLength; i++) {
         const day = startDate.add(i, 'day').add(1, 'hour');// 加1小时是因为防止边界判断
-        let className = '', index = day.format('DD'), name = index;
+        let className = '', index = day.format('DD'), name = '';
         const trainingsDay = trainings.value.find(t => day.isAfter(t.startDate) && day.isBefore(t.endDate));
         const dutyDay = duties.value.filter(t => day.isSame(t.flightDate, 'day'));
         const absenceDay = absences.value.find(t => day.isAfter(t.startDate) && day.isBefore(t.endDate));
         let data;
         if (trainingsDay) {
-            name = '培';
+            name = 'learn';
             className = 'training';
             data = trainingsDay;
         } else if (dutyDay.length) {
-            name = '飞';
+            name = 'dep';
             className = 'duty';
             data = dutyDay;
         } else if (absenceDay) {
-            name = '休';
+            // 在absenceReasons中查找key
+            for (let key in absenceReasons) {
+                if (absenceReasons[key].includes(absenceDay.title)) {
+                    name = key;
+                    break;
+                }
+            }
+            console.log('absenceReasons', absenceReasons, absenceDay)
+            // name = '休';
             className = 'absence';
             data = absenceDay;
         }
-        if (day.isSame(dayjs(), 'day')) {
-            className += ' today';
-        }
+        // if (day.isSame(dayjs(), 'day')) {
+        //     className = 'today';
+        // }
         arr.push({ index, name, className, data });
     }
     return arr;
@@ -74,43 +94,23 @@ function showDetail(day: { name: string, className: string, data: any }) {
         })
     }
 }
+
 watch(() => props.pcode, () => {
     if (!props.pcode) return;
-    const userData = { userId: props.pcode, idType: 'pcode' }
-    const dateData = { startDate: props.startDate, endDate: props.endDate }
+    const data = { userId: props.pcode, idType: 'pcode', startDate: props.startDate, endDate: props.endDate }
     Promise.allSettled([
         airportStore.fetchAirports(),
-        api(CONFIG.url.pilotTraining, { ...userData, ...dateData }).then(res => trainings.value = res || []),
-        api(CONFIG.url.pilotDuty, { ...userData, ...dateData }).then(res => duties.value = res || []),
-        api(CONFIG.url.pilotAbsence, { ...userData, ...dateData }).then(res => absences.value = res || []),
-    ]).then((arr) => {
-        console.log('获取信息', arr, trainings.value, duties.value, absences.value);
-    }).catch(err => console.log('获取信息', err));
-})
-
-// // 获取飞行员轨迹
-
-// watch(() => [props.userId, props.dateRange], (userId) => {
-//     const startDate = dayjs(props.dateRange[0]).toDate();
-//     const endDate = dayjs(props.dateRange[1]).toDate();
-//     fetchFlightTracks(startDate, endDate, props.userId);
-// }, { deep: true, immediate: true });
-// // 获取机场信息
-// const fetchAirports = async () => {
-//     loading.value = true;
-//     error.value = '';
-//     try {
-//         const res = await airportStore.fetchAirports ;
-//         // airports.value = res;
-//     } catch (err) {
-//         error.value = '获取机场信息失败';
-//     } finally {
-//         loading.value = false;
-//     }
-// };
+        api(CONFIG.url.pilotTraining, data).then(res => trainings.value = res || []),
+        api(CONFIG.url.pilotDuty, data).then(res => duties.value = res || []),
+        api(CONFIG.url.pilotAbsence, data).then(res => absences.value = res || []),
+    ]).then((arr) => console.log('获取信息', arr, trainings.value, duties.value, absences.value))
+        .catch(err => console.warn('错误', err));
+}, { immediate: true, deep: true })
 
 </script>
 <style lang="less" scoped>
+@import '@/css/base.less';
+
 .flight-tracks-wrapper {
 
     display: grid;
@@ -139,17 +139,21 @@ watch(() => props.pcode, () => {
         &.today {
             border: solid 1px red;
         }
+
+        &.training {
+            background-color: #9ba9fa;
+            color: #333;
+        }
+
+        &.duty {
+            background-color: #fc9c9c;
+            color: #333;
+        }
+
+        &.absence {
+            background-color: #91e28e;
+            color: #333;
+        }
     }
 }
-
-
-// .flight {
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: space-between;
-//     padding: 5px auto;
-
-//     .no {
-//         color: #aaa;
-//     }
-// }</style>
+</style>
