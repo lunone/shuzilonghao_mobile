8a105751e63db1f8685cd2d331625837773beb1b
zhilun <lunone@qq.com>
2025-10-13 10:33:03 +0800

    fix: 之前utils/directive删除的擦屁股工作

--- src/main.js
+++ src/main.js
@@ -6,25 +6,18 @@ import Vue from 'vue'
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
-const app = new Vue({
-    ...App
-})
+const app = new Vue(App)
 app.$mount()
 // #endif
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue';
-import { permissionDirective, roleDirective } from './utils/directives';
 import router from './utils/router';
 import pagesJson from './pages.json';
 
 export function createApp() {
     const app = createSSRApp(App);
     app.use(Pinia.createPinia());
-
-    // 注册权限指令
-    app.directive('permission', permissionDirective);
-    app.directive('role', roleDirective);
 
     // 初始化路由守卫配置
     router.setConfig({
